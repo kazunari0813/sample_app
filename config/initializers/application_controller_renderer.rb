0b2ae8341dbbfbd1f4be89ba2f6f3e3c4951afbd
takahashikazunari <haru1529@iclou.com>
2020-07-24 14:53:44 +0900

    [finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '9b9aa2dde11981dffc21007aa995a50fe06ff2f8a4f5cc4feb01c76969cdfa2bd5396001da97ef25eac4f77020ae581270d8448e49264a4bec063e5b84d46751'
